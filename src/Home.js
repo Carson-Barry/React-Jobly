4dcbcabc90ab45574886ee173516cdb8a02ea835
Carson Barry <carsonbarry93@gmail.com>
2023-10-12 17:12:12 -0700

    added landing page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
     return (
         <div className="Home">
             <h1 className="Home-title">Jobly</h1>
             <p className="Home-description">All the jobs in one convenient place.</p>
+            <Link to="/signup" className="Home-button">Sign Up</Link>
+            <Link to="/login" className="Home-button">Login</Link>
         </div>
     );
 }
